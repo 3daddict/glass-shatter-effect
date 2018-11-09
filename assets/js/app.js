30cb08bf45a21778f9c323f518bacb0e0d2870d2
Michael Salvati <print@3daddict.com>
2018-11-09 10:32:01 -0800

    different glass img files per crackcount

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,21 +10,31 @@ function glassCrack() {
   var preLoad = document.getElementsByClassName("preload")[0];
   preLoad.style.display = "none";
   function createCrack(top, left) {
-    crackCount += 1;
     // var className = "crack" + eval(crackCount);
     var className = "crack" + crackCount;
     var newDiv = document.createElement("div");
     newDiv.setAttribute("class", className + " crack-container");
+    crackCount ++;
 
     var innerCrack = document.createElement("div");
-    innerCrack.setAttribute("class", "crack");
+    console.log(crackCount);
+    if (crackCount === 1) {
+      innerCrack.setAttribute("class", "crack");
+    }
+    if (crackCount === 2) {
+      innerCrack.setAttribute("class", "crack-3");
+    }
+    if (crackCount === 3) {
+      innerCrack.setAttribute("class", "crack-2");
+    } 
+    
     newDiv.appendChild(innerCrack);
 
     document.body.appendChild(newDiv);
 
     newDiv.style.top = top + "px";
     newDiv.style.left = left + "px";
-    console.log(crackCount);
+    
   }
 
   window.addEventListener("click", function(e) {
