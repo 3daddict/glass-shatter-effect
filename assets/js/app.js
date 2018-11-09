a04bda963b33fbe68bf624dd0026c01829168261
3DAddict <print@3daddict.com>
2018-11-08 20:51:07 -0800

    basic crack function created on dom

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,35 @@
 $(document).ready(InitApp);
 
-function InitApp(){
-    console.log('App Ready')
+function InitApp() {
+  console.log("App Ready");
+  glassCrack();
+}
+
+function glassCrack() {
+  var crackCount = 0;
+  
+  var preLoad = document.getElementsByClassName("preload")[0];
+  preLoad.style.display = "none";
+  function createCrack(top, left) {
+    crackCount++;
+    // var className = "crack" + eval(crackCount);
+    var className = "crack" + crackCount;
+    var newDiv = document.createElement("div");
+    newDiv.setAttribute("class", className + " crack-container");
+
+    var innerCrack = document.createElement("div");
+    innerCrack.setAttribute("class", "crack");
+    newDiv.appendChild(innerCrack);
+
+    document.body.appendChild(newDiv);
+
+    newDiv.style.top = top + "px";
+    newDiv.style.left = left + "px";
+
+    console.log(crackCount);
+  }
+
+  window.addEventListener("click", function(e) {
+    createCrack(e.pageY, e.pageX);
+  });
 }
