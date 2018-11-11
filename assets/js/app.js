c975a40cb26a3692afd86fb7a3fdfb44f6fae0c0
Michael Salvati <print@3daddict.com>
2018-11-10 16:18:15 -0800

    explode function and image overlay working

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,49 +2,61 @@ $(document).ready(InitApp);
 
 function InitApp() {
   console.log("App Ready");
+  document.getElementById("target").style.height = window.innerHeight + "px";
   glassCrack();
 }
 
 function glassCrack() {
-  var crackCount = 0;
-  var preLoad = document.getElementsByClassName("preload")[0];
+  let crackCount = 0;
+  let preLoad = document.getElementsByClassName("preload")[0];
   preLoad.style.display = "none";
-  function createCrack(top, left) {
-    // var className = "crack" + eval(crackCount);
-    var className = "crack" + crackCount;
-    var newDiv = document.createElement("div");
-    newDiv.setAttribute("class", className + " crack-container");
-    crackCount ++;
 
-    var innerCrack = document.createElement("div");
+  function createCrack(top, left) {
+    let className = "crack" + crackCount;
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", className + " crack-container");
+    crackCount++;
+
+    let innerCrack = document.createElement("div");
     console.log(crackCount);
     if (crackCount === 1) {
       innerCrack.setAttribute("class", "crack");
     }
     if (crackCount === 2) {
-      innerCrack.setAttribute("class", "crack-3");
+      innerCrack.setAttribute("class", "crack-2");
     }
     if (crackCount === 3) {
-      innerCrack.setAttribute("class", "crack-2");
-      setTimeout(() => {
-        console.log('glass should explode now')
-      },300)
-    } 
-    
+      $(".crack-container").remove();
+      explode();
+      $(".explode-wrapper").fadeOut(1000);
+	}
+	if (crackCount === 4) {
+		return;
+	}
     newDiv.appendChild(innerCrack);
-
-    document.body.appendChild(newDiv);
-
+    document.getElementById("target").appendChild(newDiv);
     newDiv.style.top = top + "px";
     newDiv.style.left = left + "px";
-    
   }
 
   window.addEventListener("click", function(e) {
     createCrack(e.pageY, e.pageX);
   });
+
 }
 
-function explodeglass() {
-	$("#target").explode({"minWidth":3,"maxWidth":12,"radius":235,"minRadius":15,"release":true,"fadeTime":300,"recycle":false,"recycleDelay":500,"explodeTime":1000,"round":false,"minAngle":5000,"maxAngle":360,"gravity":2,"groundDistance":236});
+/* Function to explode the glassTexture.png */
+function explode() {
+  $("#target-img").explodeRestore();
+  $("#target-img").explode({
+    maxWidth: 12,
+    minWidth: 30,
+    radius: 300,
+    release: false,
+    recycle: false,
+    explodeTime: 78,
+    canvas: true,
+    maxAngle: 360,
+    gravity: true
+  });
 }
